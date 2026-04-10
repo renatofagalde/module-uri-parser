3e665fbe5ee8bdebaf045ad4d416a12c301f16a9
Renato Fagalde <renato@likwi.com.br>
2026-04-09 22:39:50 -0300

    Enhance URI parsing by adding singularization for resource names

--- uri_parser.go
+++ uri_parser.go
@@ -14,13 +14,25 @@ var uuidRegex = regexp.MustCompile(
 	`[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}`,
 )
 
+var irregularPlurals = map[string]string{
+	"companies":  "company",
+	"categories": "category",
+}
+
+func singularize(word string) string {
+	if singular, ok := irregularPlurals[word]; ok {
+		return singular
+	}
+	return strings.TrimSuffix(word, "s")
+}
+
 func Parse(method, uri string) Result {
 	parts := strings.Split(uri, "/")
 
 	for i, part := range parts {
 		if uuidRegex.MatchString(part) && i > 0 {
-			resource := strings.TrimSuffix(parts[i-1], "s")
-			parts[i] = "id_" + resource
+			resource := singularize(parts[i-1])
+			parts[i] = "{" + resource + "_id}"
 		}
 	}
 
