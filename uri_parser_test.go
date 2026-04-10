f21861ea292071b326bb524b8fa5a1a54c4265f7
Renato Fagalde <renato@likwi.com.br>
2026-04-09 22:39:55 -0300

    Expand URI parsing tests with additional cases and updated parameterized formats

--- uri_parser_test.go
+++ uri_parser_test.go
@@ -8,9 +8,13 @@ func TestParse(t *testing.T) {
 		uri    string
 		want   string
 	}{
-		{"GET", "/pessoas/4152bd3d-9121-4889-b683-6ee72b8e3719", "/pessoas/id_pessoa"},
-		{"POST", "/pessoas/4152bd3d-9121-4889-b683-6ee72b8e3719/telefones", "/pessoas/id_pessoa/telefones"},
-		{"GET", "/pessoas/4152bd3d-9121-4889-b683-6ee72b8e3719/enderecos/65020de3-8a6c-416f-97d2-9755b0111bdc", "/pessoas/id_pessoa/enderecos/id_endereco"},
+		{"GET", "/pessoas/4152bd3d-9121-4889-b683-6ee72b8e3719", "/pessoas/{pessoa_id}"},
+		{"POST", "/pessoas/4152bd3d-9121-4889-b683-6ee72b8e3719/telefones", "/pessoas/{pessoa_id}/telefones"},
+		{"GET", "/pessoas/4152bd3d-9121-4889-b683-6ee72b8e3719/enderecos/65020de3-8a6c-416f-97d2-9755b0111bdc", "/pessoas/{pessoa_id}/enderecos/{endereco_id}"},
+		{"GET", "/companies/4152bd3d-9121-4889-b683-6ee72b8e3719/categories/65020de3-8a6c-416f-97d2-9755b0111bdc", "/companies/{company_id}/categories/{category_id}"},
+		{"GET", "/cam/users/4152bd3d-9121-4889-b683-6ee72b8e3719/profiles", "/cam/users/{user_id}/profiles"},
+		{"GET", "/cam/companies/4152bd3d-9121-4889-b683-6ee72b8e3719/modules", "/cam/companies/{company_id}/modules"},
+		{"PATCH", "/cms/apolices/4152bd3d-9121-4889-b683-6ee72b8e3719/lives", "/cms/apolices/{apolice_id}/lives"},
 	}
 
 	for _, tt := range tests {
